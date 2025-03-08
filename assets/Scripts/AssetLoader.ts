f38ca0480fbc1443c6eb2a8f8b89355db1aa3073
anohis <38350932+anohis@users.noreply.github.com>
2025-03-08 12:02:19 +0800

    add asset loader

---
+++ assets/Scripts/AssetLoader.ts
@@ -0,0 +1,27 @@
+import { Asset, __private, resources } from 'cc';
+
+type AssetType<T> = __private.__types_globals__Constructor<T>;
+
+export interface IAssetLoader
+{
+    Load<T extends Asset>(path: string, type: AssetType<T>): Promise<T>;
+}
+
+export class AssetLoader implements IAssetLoader
+{
+    public Load<T extends Asset>(path: string, type: AssetType<T>): Promise<T>
+    {
+        let resolveFunc!: (value: T) => void;
+        const promise = new Promise<T>(resolve => resolveFunc = resolve);
+        resources.load(path, type, (err, prefab) =>
+        {
+            if (err != null)
+            {
+                throw err;
+            }
+            resolveFunc(prefab);
+        });
+
+        return promise;
+    }
+}
